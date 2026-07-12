0ba5ca3f3ea23d4442b465b0be17840ba139c173
ROWWWDY <igrowdygaming@gmail.com>
2026-07-12 19:46:25 +0530

    Update session.js

--- api/admin/session.js
+++ api/admin/session.js
@@ -1,10 +1,12 @@
 const { getSession } = require('../_lib/auth');
+const { capabilitiesFor } = require('../_lib/permissions');
 
 module.exports = async (req, res) => {
   const session = getSession(req);
   res.status(200).json({
     isAdmin: !!session,
     username: session ? session.username : null,
-    permRole: session ? session.permRole : null
+    permRole: session ? session.permRole : null,
+    capabilities: session ? capabilitiesFor(session.permRole) : []
   });
 };
